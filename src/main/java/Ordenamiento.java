13d7949d04d72dc9b720efd52fbd4e5a48249a29
EstMiguelCely <est.miguela.cely@unimilitar.edu.co>
2026-04-07 10:43:38 -0500

    Update Ordenamiento.java
    Se añadió la opción de poder ordenar la lista por medio del método de ordenamiento de inserción y se mantuvo la opción de burbuja, ahora se escoge por medio de un menú de switch case antes de dar el resultado.

--- src/main/java/Ordenamiento.java
+++ src/main/java/Ordenamiento.java
@@ -14,23 +14,50 @@ public class Ordenamiento {
             System.out.print("Ingrese el numero " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
         }
+        System.out.println("Seleccione el metodo de ordenamiento:");
+        System.out.println("1. Burbuja");
+        System.out.println("2. Insercion");
+        int opcion = sc.nextInt();
 
-        ordenar(arr);
+        switch (opcion) {
+            case 1:
+                ordenar(arr);
+                break;
+            case 2:
+                insercion(arr);
+                break;
+            default:
+                System.out.println("Opcion invalida");
+                return;
+        }
 
         System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
 
         sc.close();
     }
 
+
     private static void ordenar(int v[]) {
         for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length ; j++) {
+            for (int j = i+1; j < v.length -i -1; j++) {
                 if (v[i] > v[j]) {
                     int w = v[j];
                     v[j] = v[i];
                     v[i] = w;
                 }
             }
+        }
+    }
+    private static void insercion(int v[]) {
+        for (int i = 1; i < v.length; i++) {
+            int key = v[i];
+            int j = i - 1;
+
+            while (j >= 0 && v[j] > key) {
+                v[j + 1] = v[j];
+                j--;
+            }
+            v[j + 1] = key;
         }
     }
 }
