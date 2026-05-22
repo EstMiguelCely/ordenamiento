a06d22fa95527f57f68b8c5ff45abc2e10f3e537
EstMiguelCely <est.miguela.cely@unimilitar.edu.co>
2026-05-21 22:16:32 -0500

    Update Ordenamiento.java
    Revisión final.

--- src/main/java/Ordenamiento.java
+++ src/main/java/Ordenamiento.java
@@ -1,46 +1,35 @@
-import java.util.Scanner;
-import java.util.Arrays;
+package com.proyecto.ordenamiento;
+
+import java.util.*;
 
 public class Ordenamiento {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Ingrese la cantidad de numeros: ");
+        Random rand = new Random();
+        System.out.print("Cuantos numeros deseas ingresar?: ");
         int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Ingrese el numero " + (i + 1) + ": ");
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Seleccione el metodo de ordenamiento:");
-        System.out.println("1. Burbuja");
-        System.out.println("2. Insercion");
-        int opcion = sc.nextInt();
-
-        switch (opcion) {
-            case 1:
-                ordenar(arr);
-                break;
-            case 2:
-                insercion(arr);
-                break;
-            default:
-                System.out.println("Opcion invalida");
-                return;
-        }
-
-        System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
-
+for (int i = 0; i < n; i++) {
+   
+    arr[i] = rand.nextInt(100); 
+}
+        
+        ordenar(arr);
+       System.out.println("Arreglo ordenado: " + java.util.Arrays.toString(arr));
         sc.close();
     }
 
-
+    
+    //metodo burbuja 
+    /*
     private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length -i -1; j++) {
+        // i llega hasta el penúltimo elemento
+        for (int i = 0; i < v.length - 1; i++) {
+            // j debe llegar hasta el final del arreglo (v.length)
+            for (int j = i + 1; j < v.length; j++) { 
                 if (v[i] > v[j]) {
+                    // Intercambio de valores
                     int w = v[j];
                     v[j] = v[i];
                     v[i] = w;
@@ -48,16 +37,18 @@ public class Ordenamiento {
             }
         }
     }
-    private static void insercion(int v[]) {
-        for (int i = 1; i < v.length; i++) {
-            int key = v[i];
-            int j = i - 1;
-
-            while (j >= 0 && v[j] > key) {
-                v[j + 1] = v[j];
-                j--;
-            }
-            v[j + 1] = key;
-        }
-    }
+    */
+       //metodo por incersion
+  private static void ordenar(int v[]) {
+    for (int i = 1; i < v.length; i++) {
+        int clave = v[i]; 
+        int j = i - 1;
+while (j >= 0 && v[j] > clave) {
+            v[j + 1] = v[j];
+            j--;
+}
+v[j + 1] = clave;
+}
+}
+  
 }
